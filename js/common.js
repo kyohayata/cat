5591371595d9e4210e981148aac067e86cc59dc6
uhei <50701988+kyohayata@users.noreply.github.com>
2020-08-05 14:26:32 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,14 +1,30 @@
 /*画面をスライドすると現れる追従ボタン */
-$(function(){
-  jQuery(window).on("scroll", function() {
-    if (700 < jQuery(this).scrollTop()) {  //700より大きくすると画面の一番上からボタンが現れるまでの位置を下げることができる
-      jQuery(".floating").css({
-        display: "block",
-      });
-    } else {
-      jQuery(".floating").css({
-        display: "none",
-      });
-    }
-  });
+$(window).on("scroll", function() {
+  if (100 < $(this).scrollTop()) {
+    $(".floating").css({
+      display: "block",
+    });
+  } else {
+    $(".floating").css({
+      display: "none",
+    });
+  }
 });
+
+//ここからスムーススクロール
+$(function(){
+  // #で始まるアンカーをクリックした場合に処理
+      $('a[href^="#"]').click(function() {
+          // スクロールの速度
+          var speed = 500; // ミリ秒
+          // 移動先を取得
+          var href= $(this).attr("href");
+          var target = $(href == "#" || href == "" ? 'html' : href);
+          // 移動先を数値で取得
+          var position = target.offset().top;
+          // スムーススクロール
+          $('body,html').animate({scrollTop:position}, speed, 'swing');
+          return false;
+      });
+  });
+//ここまでスムーススクロール
